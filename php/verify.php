464ac9660678a154cd30689067c27f330d95d4b6
zepphyx13 <gungnands@gmail.com>
2022-04-21 09:07:52 +0800

    nambahin cookie ke system login

--- php/verify.php
+++ php/verify.php
@@ -2,6 +2,17 @@
 session_start();
 require 'backend.php';
 
+if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
+    $Name = $_COOKIE['id'];
+    $key = $_COOKIE['key'];
+
+    $result = mysqli_query($konek, "SELECT nama FROM pelanggan WHERE id=$id");
+    $row = mysqli_fetch_assoc($result);
+    if ($key == hash('sha256', $row['nama'])) {
+        $_SESSION['login'] || $_SESSION['admin'] = true;
+    }
+}
+
 if (isset($_POST["login"])) {
     $email = $_POST["email"];
     $password = $_POST["password"];
@@ -14,14 +25,20 @@ if (isset($_POST["login"])) {
         if (password_verify($password, $row["password"])) {
 
             if ($row['lvl'] == "user" && $_SESSION["login"] = true) {
-
+                if (isset($_POST['ingat'])) {
+                    setcookie('Name', $row['id'], time() + 10800);
+                    setcookie('velue', hash('sha256', $row['$username']), time() + 10800);
+                }
 
                 $_SESSION['email'] = $email;
                 $_SESSION['lvl'] = "user";
 
                 header("location:../user/");
             } else if ($row['lvl'] == "admin" && $_SESSION["admin"] = true) {
-
+                if (isset($_POST['ingat'])) {
+                    setcookie('Name', $row['id'], time() + 43200);
+                    setcookie('velue', hash('sha256', $row['$username']), time() + 43200);
+                }
                 $_SESSION['email'] = $email;
                 $_SESSION['lvl'] = "admin";
 
