b717f67e7cee33886d5be967faef78199de17189
zepphyx13 <gungnands@gmail.com>
2022-04-21 09:57:09 +0800

    singup

--- register.php
+++ register.php
@@ -32,7 +32,7 @@ if (isset($_POST["register"])) {
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-6 text-center mb-5">
-                    <h2 class="heading-section">Sing up</h2>
+                    <h2 class="heading-section">Sign up</h2>
                 </div>
             </div>
             <div class="row justify-content-center">
