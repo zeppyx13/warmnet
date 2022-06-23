25c5985a72e9a2306206c08df0205dfa00fddc3c
zepphyx13 <gungnands@gmail.com>
2022-06-23 10:24:17 +0800

    fixing file config and improved title

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if (isset($_SESSION["login"])) {
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-   <title>Home</title>
+   <title>Warm-net||Home</title>
    <meta name="keywords" content="">
    <meta name="description" content="">
    <meta name="author" content="">
